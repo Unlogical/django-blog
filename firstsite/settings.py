40368a239beffc0fcf797bebbefcc82a2c8b7662
Unlogical <sakujo8744@mail.ru>
2018-02-22 17:10:51 +0300

    round buttons after element added

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -115,6 +115,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+#Redirect
+LOGIN_REDIRECT_URL = '/'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
