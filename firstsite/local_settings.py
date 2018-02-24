28daede8b19e6f209665b015ed66680d9c0869c0
Unlogical <sakujo8744@mail.ru>
2018-02-24 03:32:08 +0300

    heroku first commit

---
+++ firstsite/local_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'orangeblog_db',
+        'USER': 'admin',
+        'PASSWORD': 'adminadmin',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
