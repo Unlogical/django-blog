0294c6745685b617e88b2aad05252fff0e9a7b0a
Unlogical <sakujo8744@mail.ru>
2018-02-25 23:55:14 +0300

    starter settings

--- firstsite/local_settings.py
+++ firstsite/local_settings.py
@@ -1,11 +1,25 @@
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'orangeblog_db',
-        'USER': 'admin',
-        'PASSWORD': 'adminadmin',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DEBUG = True
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'orangeblog_db',
+#        'USER': 'admin',
+#        'PASSWORD': 'adminadmin',
+#        'HOST': 'localhost',
+#        'PORT': '',
+#    }
+#}
 
