d5a9f6c2d40de1812777715c47ad9fdc09ae5e0f
Unlogical <sakujo8744@mail.ru>
2018-02-26 00:07:33 +0300

    additional files and changes for Heroku

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -126,8 +126,8 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/blog/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
